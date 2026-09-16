fe2b9998ab3d57e11b5d6bc82cb54701a086e92e
Tim Huse <thuse96@gmail.com>
2026-09-17 01:50:00 +0200

    Fix Python 3.14 compatibility issues, add migrations

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
